2bc2de59cd5008b285b391814455e105a3595ccf
saicem <saicem@qq.com>
2022-10-11 22:55:02 +0800

    『fix』修复 PlainText 返回

--- README.md
+++ README.md
@@ -14,13 +14,25 @@ main.py 用于 fastapi 部署
 
 样例
 
-<img src="docs/course.png" width="40%">
+<img src="docs/course.png" width="35%" alt="课表示例图片">
 
 接口参考,运行 [main.py](main.py) 后查看 http://127.0.0.1:8000/docs
 
 ## 部署
 
-Dockerfile 直接用就好，[config.py](config.py) 需要更改一下，字体呀，开学日期呀。
+[Dockerfile](Dockerfile) 直接用就好，[config.py](config.py) 需要更改一下，字体呀，开学日期呀。
+
+也可以用已经构建好的镜像
+https://hub.docker.com/repository/docker/saicem/fast-fetcher
+
+修改一下，本地的开放端口
+
+```shell
+docker pull saicem/fast-fetcher
+docker run -d -p 8000:8000 saicem/fast-fetcher 
+```
+
+字体用的是[霞鹜文楷](https://github.com/lxgw/LxgwWenKai)，可以自行更改
 
 ## 爬虫
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import io
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
-from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.responses import StreamingResponse, JSONResponse, PlainTextResponse
 from pydantic import BaseModel
 import uvicorn
 
@@ -41,7 +41,7 @@ class CoursePngForm(LoginForm):
 def electric(form: ElectricForm):
     ias = Ias(form.username, form.password)
     if not ias.login():
-        return Response(content="登录失败,检查账密是否正确！", status_code=401)
+        return PlainTextResponse(content="登录失败,检查账密是否正确！", status_code=401)
 
     res = ias.fetch_electric_fee(form.meterId, form.factoryCode)
     res_json = res.json()
@@ -62,7 +62,7 @@ def electric(form: ElectricForm):
 def books(form: LoginForm):
     ias = Ias(form.username, form.password)
     if not ias.login():
-        return Response(content="登录失败,检查账密是否正确！", status_code=401)
+        return PlainTextResponse(content="登录失败,检查账密是否正确！", status_code=401)
     res = ias.fetch_books()
     raw_books = res.json()['list']
 
@@ -80,11 +80,11 @@ def books(form: LoginForm):
 @app.post("/course/png")
 def course_png(form: CoursePngForm):
     if not 1 <= form.week <= 20:
-        return Response(content="周次应该在 1~20", status_code=400)
+        return PlainTextResponse(content="周次应该在 1~20", status_code=400)
 
     ias = Ias(form.username, form.password)
     if not ias.login():
-        return Response(content="登录失败,检查账密是否正确！", status_code=401)
+        return PlainTextResponse(content="登录失败,检查账密是否正确！", status_code=401)
 
     res = ias.fetch_jwc_main_page()
     courses = parse_courses_from_main_page(res.text)
@@ -104,7 +104,7 @@ def course_png(form: CoursePngForm):
 def course_json(form: LoginForm):
     ias = Ias(form.username, form.password)
     if not ias.login():
-        return Response(content="登录失败,检查账密是否正确！", status_code=401)
+        return PlainTextResponse(content="登录失败,检查账密是否正确！", status_code=401)
     res = ias.fetch_jwc_main_page()
     courses = parse_courses_from_main_page(res.text)
     return JSONResponse(content=jsonable_encoder({"data": courses}))
@@ -114,7 +114,7 @@ def course_json(form: LoginForm):
 def course_ical(form: LoginForm):
     ias = Ias(form.username, form.password)
     if not ias.login():
-        return Response(content="登录失败,检查账密是否正确！", status_code=401)
+        return PlainTextResponse(content="登录失败,检查账密是否正确！", status_code=401)
     res = ias.fetch_jwc_main_page()
     courses = parse_courses_from_main_page(res.text)
     cal = IcalWriter(TERM_START_DAY, courses).make_ical_from_course()
