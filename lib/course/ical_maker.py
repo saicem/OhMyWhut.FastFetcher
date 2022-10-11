6d57af46c460f1bd9198b0cb74d0e196ae7bbec9
saicem <saicem@qq.com>
2022-10-11 18:02:01 +0800

    『feat』增加课表 ical, json 接口

---
+++ lib/course/ical_maker.py
@@ -0,0 +1,59 @@
+import io
+import uuid
+from datetime import datetime, timedelta
+
+from lib.course.course import Course
+
+START_TIME_DIC = {
+    1: timedelta(hours=8),
+    3: timedelta(hours=9, minutes=55),
+    6: timedelta(hours=14),
+    9: timedelta(hours=16, minutes=45),
+    11: timedelta(hours=19),
+}
+
+END_TIME_DIC = {
+    2: timedelta(hours=9, minutes=35),
+    4: timedelta(hours=11, minutes=30),
+    5: timedelta(hours=12, minutes=20),
+    7: timedelta(hours=15, minutes=35),
+    8: timedelta(hours=15, minutes=25),
+    12: timedelta(hours=20, minutes=35),
+    13: timedelta(hours=21, minutes=25),
+}
+
+
+def compute_start_day(term_start_day: datetime, week: int, dow: int) -> datetime:
+    return term_start_day + (week * 7 + (6 if dow == 0 else dow - 1)) * timedelta(days=1)
+
+
+class IcalWriter:
+    def __init__(self, term_start_date: datetime, courses: list[Course]):
+        self.ical = io.StringIO()
+        self.courses = courses
+        self.term_start_date = term_start_date
+
+    def write_event(self, course: Course):
+        start_day = compute_start_day(self.term_start_date, course.StartWeek, course.DayOfWeek)
+        date_start = start_day + START_TIME_DIC[course.StartSection] - timedelta(hours=8)
+        date_end = start_day + END_TIME_DIC[course.EndSection] - timedelta(hours=8)
+        start_day.strftime("%Y%m%dT%H%M%SZ")
+        self.ical.write("BEGIN:VEVENT\n")
+        self.ical.write(f"UID:{uuid.uuid4()}\n")
+        self.ical.write(f"SUMMARY:{course.Name}\n")
+        self.ical.write(f"LOCATION:{course.Room}\n")
+        self.ical.write(f"DESCRIPTION:{course.Teacher}\n")
+        self.ical.write(f"DTSTART:{date_start.strftime('%Y%m%dT%H%M%SZ')}\n")
+        self.ical.write(f"DTEND:{date_end.strftime('%Y%m%dT%H%M%SZ')}\n")
+        self.ical.write(f"RRULE:FREQ=WEEKLY;INTERVAL=1;COUNT={course.EndWeek - course.StartWeek + 1}\n")
+        self.ical.write("END:VEVENT\n")
+
+    def make_ical_from_course(self):
+        self.ical = io.StringIO()
+        self.ical.write("BEGIN:VCALENDAR\n")
+        self.ical.write("VERSION:2.0\n")
+        self.ical.write(f"PRODID:{uuid.uuid4()}\n")
+        for course in self.courses:
+            self.write_event(course)
+        self.ical.write("END:VCALENDAR")
+        return self.ical
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import io
 
 from fastapi import FastAPI, Response
-from fastapi.responses import StreamingResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import StreamingResponse, JSONResponse
 from pydantic import BaseModel
 import uvicorn
 
 from config import *
 
 from lib.course.course import parse_courses_from_main_page
+from lib.course.ical_maker import IcalWriter
 from lib.course.picgen import CourseDrawer
 from lib.ias import Ias
 
@@ -87,6 +89,29 @@ def course_png(form: CoursePngForm):
     png.save(buf, format='png')
     buf.seek(0)
     return StreamingResponse(content=buf, media_type="image/png")
+
+
+@app.post("/course/json")
+def course_json(form: LoginForm):
+    ias = Ias(form.username, form.password)
+    if not ias.login():
+        return Response(content="登录失败,检查账密是否正确！", status_code=401)
+    res = ias.fetch_jwc_main_page()
+    courses = parse_courses_from_main_page(res.text)
+    return JSONResponse(content=jsonable_encoder({"data": courses}))
+
+
+@app.post("/course/ical")
+def course_ical(form: LoginForm):
+    ias = Ias(form.username, form.password)
+    if not ias.login():
+        return Response(content="登录失败,检查账密是否正确！", status_code=401)
+    res = ias.fetch_jwc_main_page()
+    courses = parse_courses_from_main_page(res.text)
+    cal = IcalWriter(TERM_START_DAY, courses).make_ical_from_course()
+    cal.seek(0)
+    return StreamingResponse(content=cal, media_type="text/calendar",
+                             headers={'Content-Disposition': 'attachment; filename="courses.ics"'})
 
 
 if __name__ == "__main__":
