146e0f07a971a0b4e1e8cff9943fc60a606f58ea
saicem <saicem@qq.com>
2023-02-20 09:20:10 +0800

    『fix』一个格子有多节课的情况解析有误

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from PIL import ImageFont
 
 TERM_START_DATE = datetime.date(
-    datetime.strptime(os.environ.get("TERM_START_DATE", "2022-08-29"), "%Y-%m-%d")
+    datetime.strptime(os.environ.get("TERM_START_DATE", "2023-02-20"), "%Y-%m-%d")
 )
 IMAGE_TTF = ImageFont.truetype(
     os.environ.get("COURSE_TTF", "../data/fonts/LXGWWenKaiMono-Regular.ttf"), 24
--- src/services/course_parser.py
+++ src/services/course_parser.py
@@ -13,23 +13,22 @@ def parse_courses_from_main_page(html: str) -> list[Course]:
     tds: list[_Element] = root.xpath('//*[@id="weekTable"]/tbody/tr/td[@id]')
     for idx, td in enumerate(tds):
         # 需要更多的样本来处理、验证解析是否正确、如解析错误请发起 issue，并提供源 html
-        # 形势与政策(第10-13周6-7节,王珺老师,东教-合一)
-        match = re.match(
-            "(.+?)\\(第(\\d+)-(\\d+)周(\\d+)-(\\d+)节,(.+)老师,(.+)\\)",
-            re.sub("\\s", "", td.text),
+        # '_形势与政策(第07-07周6-7节_冯蓓_老师_弘毅(教4)-501)_形势与政策(第05-06周6-8节_冯蓓_老师_弘毅(教4)-501)_'
+        matches = re.finditer(
+            "_(.+?)\\(第(\\d+)-(\\d+)周(\\d+)-(\\d+)节_(.+?)_老师_(.+?)\\)(?=_)",
+            re.sub("[\\s,]+", "_", td.text),
         )
-        if match is None:
-            continue
-        courses.append(
-            Course(
-                name=match.group(1),
-                start_week=int(match.group(2)),
-                end_week=int(match.group(3)),
-                start_section=int(match.group(4)),
-                end_section=int(match.group(5)),
-                teacher=match.group(6),
-                room=match.group(7),
-                day_of_week=(idx + 1) % 7,
+        for match in matches:
+            courses.append(
+                Course(
+                    name=match.group(1),
+                    start_week=int(match.group(2)),
+                    end_week=int(match.group(3)),
+                    start_section=int(match.group(4)),
+                    end_section=int(match.group(5)),
+                    teacher=match.group(6),
+                    room=match.group(7),
+                    day_of_week=(idx + 1) % 7,
+                )
             )
-        )
     return courses
